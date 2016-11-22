5ef8fcc9df01849ecd7804622b85f44d3045dabd
envyhunter <seanrpearson1350@gmail.com>
2016-11-21 17:05:42 -0700

    database start

--- Novinophobia/Database.php
+++ Novinophobia/Database.php
@@ -1,1 +1,42 @@
 <?php
+//
+//
+//
+class DatabaseAdaptor {
+	// The instance variable used in every one of the functions in class DatbaseAdaptor
+	private $DB;
+	// Make a connection to an existing data based named 'quotes' that has table quotations.
+	public function __construct() {
+		$db = 'mysql:dbname=quotes;host=127.0.0.1';
+		$user = 'root';
+		$password = "";
+		try {
+			$this->DB = new PDO ( $db, $user, $password );
+			$this->DB->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		} catch ( PDOException $e ) {
+			echo ('Rick writes: Error establishing Connection!!!!');
+			exit ();
+		}
+	}
+	// Return all quotations records as an associative array.
+	public function getQuotesAsArray() {
+		$stmt = $this->DB->prepare ( "SELECT * FROM quote_info ORDER BY votes DESC, id DESC;" );
+		$stmt->execute ();
+		return $stmt->fetchAll ( PDO::FETCH_ASSOC );
+	}
+	public function updateUserPreferences($quote, $author) {
+		$stmt = $this->DB->prepare ( "INSERT INTO quote_info (id, author, quote, votes, date_added)  values(null, :author, :quote, 0, now())" );
+		$stmt->bindParam ( 'quote', $quote );
+		$stmt->bindParam ( 'author', $author );
+		$stmt->execute ();
+	}
+	public function voteUpdate($aORs, $quoteID) {
+		if(strcmp($aORs, "+") == 0){
+			$stmt = $this->DB->prepare ( "UPDATE quote_info SET votes = votes + 1 WHERE id = '$quoteID'");
+		} else {
+			$stmt = $this->DB->prepare ( "UPDATE quote_info SET votes = votes - 1 WHERE id = '$quoteID'");
+		}
+		$stmt->execute ();
+	}
+}
+?>
